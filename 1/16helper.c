2cc24ef2d331685c5802867cd0fe7f30cb7abf1e
BradGrensewich <bradgrensewich@gmail.com>
2025-08-01 15:23:10 +0700

    adders

--- 1/16helper.c
+++ 1/16helper.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 0; i < 16; i ++) {
-        printf("    Nand(a=a[%d], b=notsel, out=t%d1);\n", i, i);
-        printf("    Nand(a=sel, b=b[%d], out=t%d2);\n", i, i);
-        printf("    Nand(a=t%d1, b=t%d2, out=out[%d]);\n", i, i, i);
-        puts("");
+    for (int i = 1; i < 16; i ++) {
+        printf("    FullAdder(a=a[%d] , b=b[%d] , c=c%d , sum=out[%d] , carry=c%d );\n", i, i, i - 1, i, i);
+        
     }
 }
